a08155356da2122572cbdce5eb539386a7f63169
SrV <985693016@qq.com>
2019-04-10 00:01:30 +0800

    create ObtainIns project

---
+++ src/test/java/com/wsf/app/Client.java
@@ -0,0 +1,10 @@
+package com.wsf.app;
+
+import okhttp3.OkHttpClient;
+
+public class Client {
+
+    public static void main(String[] args) {
+        OkHttpClient client=new OkHttpClient();
+    }
+}
